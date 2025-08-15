c7f0f1d56741dd549dc77dc268d6be13e3feb756
teaongsi <angdembe.teaongsi@gmail.com>
2025-08-15 21:52:28 +0545

    Add routes

---
+++ backend/04-mongodb-connect/router/userRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { getUserByEmail, postUser, patchUser, delUser } from '../src/controller/userController.js';
+
+const userRouter = express.Router();
+
+userRouter.route("/user").post(postUser);
+userRouter.route("/user").get(getUserByEmail);
+userRouter.route("/user").patch(patchUser);
+userRouter.route("/user").delete(delUser);
+
+export default userRouter;
