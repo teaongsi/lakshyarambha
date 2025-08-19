5115cafd933c1e924c6b2b69eba2a2fea4422857
teaongsi <angdembe.teaongsi@gmail.com>
2025-08-19 15:25:57 +0545

    Add controllers

---
+++ backend/05-simple-CRUD/controllers/productController.js
@@ -0,0 +1,59 @@
+import Product from '../models/productModel.js'
+
+export const getAll = async (req, res) => {
+    try {
+        const product = await Product.find({});
+        res.status(200).json({ success: true, product });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const getOne = async (req, res) => {
+    const { id }= req.params;
+    try {
+        const product = await Product.findById(id);
+        res.status(200).json({ success: true, product });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const patchOne = async (req, res) => {
+    const { id }= req.params;
+    try {
+        const product = await Product.findByIdAndUpdate(id, req.body, {new: true});
+
+        if(!product){
+            res.status(404).json({ success: false, message: "Product not found" });
+        }
+
+        const updatedproduct = await Product.findById(id);
+        res.status(200).json({ success: true, product });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const deleteOne = async (req, res) => {
+    const { id }= req.params;
+    try {
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product){
+            res.status(404).json({ success: false, message: "Product not found" });
+        }
+        res.status(200).json({ success: true, product });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const postOne = async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json({ success: true, product });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
