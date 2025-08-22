288910cd20b11a0f1c0fe3c9d1cbd80ce63a45b2
teaongsi <angdembe.teaongsi@gmail.com>
2025-08-22 16:39:13 +0545

    Add comemnt

--- backend/05-simple-CRUD/controllers/productController.js
+++ backend/05-simple-CRUD/controllers/productController.js
@@ -28,7 +28,7 @@ export const patchOne = async (req, res) => {
             res.status(404).json({ success: false, message: "Product not found" });
         }
 
-        const updatedproduct = await Product.findById(id);
+        // const updatedproduct = await Product.findById(id);
         res.status(200).json({ success: true, product });
     } catch (err) {
         res.status(500).json({ message: err.message });
