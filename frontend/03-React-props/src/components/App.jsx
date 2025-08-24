e1ba1f252508021adc9204df0e8e0af7e0c079fa
teaongsi <angdembe.teaongsi@gmail.com>
2025-08-24 16:11:01 +0545

    Modify App.jsx

--- frontend/03-React-props/src/components/App.jsx
+++ frontend/03-React-props/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Card from "./Card.jsx";
 import { contacts } from "../contacts.js";
+import Avatar from "./Avatar.jsx";
 
 export default function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+
+      <Avatar img="https://yt3.googleusercontent.com/JDPwhh73GWjQDySQyOGgbF6xof2nf_5zJ3_djjI-kqcuqUiznHCElC5uB5l8xzvJg_u_QIWi=s900-c-k-c0x00ffffff-no-rj" />
       
        <Card name={contacts[0].name} img={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email}  />
 
