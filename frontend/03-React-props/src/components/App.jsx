55e604822dc2dd258f100ce7f227c70a6432d5bb
teaongsi <angdembe.teaongsi@gmail.com>
2025-08-24 09:29:29 +0545

    Add files

---
+++ frontend/03-React-props/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./Card.jsx";
+import { contacts } from "../contacts.js";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      
+       <Card name={contacts[0].name} img={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email}  />
+
+      <Card name={contacts[1].name} img={contacts[1].imgURL} phone={contacts[1].phone} email={contacts[1].email}  />
+
+      <Card name={contacts[2].name} img={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email}  />
+    </div>
+  );
+}
---
+++ frontend/03-React-props/src/contacts.js
@@ -0,0 +1,23 @@
+export const contacts = [
+  {
+    name: "Joey",
+    imgURL:
+      "https://imgix.ranker.com/user_node_img/3832/76638627/original/joey-tv-characters-photo-u1",
+    phone: "123456789",
+    email: "joey@mail.com"
+  },
+  {
+    name: "Marky",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvHBaDlmTRzW00NJ4frUdO4YCZ8uqw09ALMOskXfsCM5wwZM1iUfM_uGiA-7MvWNjxm2c&usqp=CAU",
+    phone: "+987 654 321",
+    email: "marky@mail.com"
+  },
+  {
+    name: "Dee Dee",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt7Ie_dnGC3mZ8ELTcpwKKOjAo_l8vHEh_ow&s",
+    phone: "+918 372 574",
+    email: "deedee@mail.com"
+  }
+];
