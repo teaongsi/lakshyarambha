218102b368184da6c4089fcbfa7ef354b1f14868
teaongsi <angdembe.teaongsi@gmail.com>
2025-08-24 16:10:03 +0545

    Add Card.jsx

--- frontend/03-React-props/src/components/Card.jsx
+++ frontend/03-React-props/src/components/Card.jsx
@@ -1,19 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar.jsx";
+import Details from "./Details.jsx";
 
 export default function Card(props) {
     return(
         <div className="card">
             <div className="top">
                 <h2 className="name">{props.name}</h2>
-                <img
-                    className="circle-img"
-                    src={props.img}
-                    alt="avatar_img"
-                />
+                <Avatar img={props.img}/>
             </div>
             <div className="bottom">
-                <p className="info">{props.phone}</p>
-                <p className="info">{props.email}</p>
+                <Details detailInfo={props.phone} />
+                <Details detailInfo={props.email} />
             </div>
     </div>
     );
