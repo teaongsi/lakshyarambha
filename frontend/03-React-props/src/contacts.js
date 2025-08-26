bead3c91db3580ff33d10a69075ccd90c13bd0c9
teaongsi <angdembe.teaongsi@gmail.com>
2025-08-26 21:55:36 +0545

    Add id

--- frontend/03-React-props/src/contacts.js
+++ frontend/03-React-props/src/contacts.js
@@ -1,5 +1,6 @@
 export const contacts = [
   {
+    id: 1,
     name: "Joey",
     imgURL:
       "https://imgix.ranker.com/user_node_img/3832/76638627/original/joey-tv-characters-photo-u1",
@@ -7,6 +8,7 @@ export const contacts = [
     email: "joey@mail.com"
   },
   {
+    id: 2,
     name: "Marky",
     imgURL:
       "https://i.pinimg.com/736x/6c/51/04/6c510478d635536d9d45b0801bf4d92b.jpg",
@@ -14,6 +16,7 @@ export const contacts = [
     email: "marky@mail.com"
   },
   {
+    id: 3,
     name: "Dee Dee",
     imgURL:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt7Ie_dnGC3mZ8ELTcpwKKOjAo_l8vHEh_ow&s",
